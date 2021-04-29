8277bdb1d09a9a3416cce0b9e96ac0f969898ec9
shumynvolodymyr <shumivova@gmail.com>
2021-04-29 11:53:17 +0300

    lesson_1

---
+++ src/character/characterComponent.js
@@ -0,0 +1,8 @@
+import './characterComponent.css'
+function CharacterComponent(props) {
+    return <div>
+        <h2>{props.description}</h2>
+        <img src={props.image} alt=""/>
+    </div>
+}
+export default CharacterComponent;
