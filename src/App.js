851d5d8df99df0ab7fdaf0d854c8ead3d5c57320
shumynvolodymyr <shumivova@gmail.com>
2021-05-08 21:05:55 +0300

    lesson_3_axios_dataService_Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import Users from "./components/users/Users";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-<Users/>
+    return (
+        <div>
 
-    </div>
-  );
+            {/*<Users/>*/}
+
+            <Router>
+                <Link to={'/users'}>to users</Link>
+                <br/>
+                <Link to={'/posts'}>to posts</Link>
+                <Switch>
+
+                    <Route path={'/users'} render={() => (<Users/>)}/>
+                    <Route path={'/posts'} render={()=>(<div> <b>404 :(</b></div>)}/>
+
+                </Switch>
+            </Router>
+
+        </div>
+    );
 }
 
 export default App;
